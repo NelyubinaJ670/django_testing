fb3a7ae7a16f605840c47459992ecefddb67a0ad
Юлия Нелюбина <loginovskaya.yuliya.677@mail.ru>
2023-06-22 17:51:44 +0900

    вроде все тесты переделала на не пустую базу

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -12,12 +12,14 @@ from news.models import Comment
 @pytest.mark.django_db
 def test_anonymous_user_cant_create_comment(client, pk_for_args, form_data):
     """ Анонимный пользователь не может создать комментарий. """
+    comments_count_control = Comment.objects.count()
     url = reverse('news:detail', args=pk_for_args)
     response = client.post(url, data=form_data)
     login_url = reverse('users:login')
     expected_url = f'{login_url}?next={url}'
     assertRedirects(response, expected_url)
-    assert Comment.objects.count() == 0
+    comments_count = Comment.objects.count()
+    assert comments_count == comments_count_control
 
 
 def test_user_can_create_comment(
@@ -27,7 +29,7 @@ def test_user_can_create_comment(
     response = author_client.post(url, data=form_data)
     new_comment = Comment.objects.get()
     assertRedirects(response, f'{url}#comments')
-    assert Comment.objects.count() == 1
+    assert Comment.objects.count() + 1
     assert new_comment.text == form_data['text']
     assert new_comment.author == author
     assert new_comment.news == news
@@ -51,16 +53,18 @@ def test_author_can_delete_comment(
     response = author_client.post(reverse('news:delete', args=pk_for_comment))
     expected_url = reverse('news:detail', args=pk_for_args) + '#comments'
     assertRedirects(response, expected_url)
-    assert Comment.objects.count() == 0
+    assert Comment.objects.count() - 1
 
 
 def test_user_cant_delete_comment_of_another_user(
         admin_client, pk_for_comment):
     """ Пользователь не может удалить чужой комментарий. """
+    comments_count_control = Comment.objects.count()
     url = reverse('news:delete', args=pk_for_comment)
     response = admin_client.post(url)
     assert response.status_code == HTTPStatus.NOT_FOUND
-    assert Comment.objects.count() == 1
+    comments_count = Comment.objects.count()
+    assert comments_count == comments_count_control
 
 
 def test_user_cant_edit_comment_of_another_user(
